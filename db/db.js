9c6a7b5693bb1a0ebea95a92417229bcb227a4c7
nandakumar999 <tnandakumar@digispheretech.in>
2025-01-04 17:23:03 +0530

    db chnages

--- db/db.js
+++ db/db.js
@@ -2,11 +2,14 @@ const Pool = require("pg").Pool;
 require('dotenv').config();
 
 const pool = new Pool({  
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5000,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,  // Default to 5432 if not provided
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // If Render requires SSL
+  },
   
   });
 
