9187dda51338ea21144d972215c7370fa8d0436b
nandakumar999 <tnandakumar@digispheretech.in>
2025-01-06 16:21:33 +0530

    added path serverfile

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
-const app = express();
+const path = require("path"); // Import the path module
 const cors = require("cors");
-const employeeRoutes=require('./routes/Employeeroutes');
-const attendenceRoutes=require('./routes/attendenceroutes');
-const adminRoutes=require('./routes/adminroutes');
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json())
+const employeeRoutes = require('./routes/Employeeroutes');
+const attendenceRoutes = require('./routes/attendenceroutes');
+const adminRoutes = require('./routes/adminroutes');
+
+const app = express();
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve static files
+app.use(express.json());
 app.use(cors());
-app.use('/employee',employeeRoutes);
-app.use('/attendence',attendenceRoutes);
-app.use('/admin',adminRoutes);
+
+app.use('/employee', employeeRoutes);
+app.use('/attendence', attendenceRoutes);
+app.use('/admin', adminRoutes);
+
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
+    console.log("Server is running on port 5000");
+});
