6fd11b196f05ebf04de9e4c7790a18781672ff9a
nandakumar999 <tnandakumar@digispheretech.in>
2025-03-24 12:10:02 +0530

    modified pumpsales reports

--- Controller/pumpsaleReports.js
+++ Controller/pumpsaleReports.js
@@ -14,16 +14,28 @@ exports.pumsalesreportsRange = async (req, res) => {
         }
 
         const query = `
-        SELECT DISTINCT ON (e.employee_id) ps.pump_sale_id, ps.bay_side, ps.fuel_type, ps.created_at, 
-               a."pumpNumber", a.operatorshift, e."employeeName", a.attendence_id, e.employee_id,psd.plus_amount,psd.short_amount,psd.shift_sales_amount,psd.pump_sale_amount
-        FROM pump_sales ps
-        JOIN attendence a ON ps.attendence_id = a.attendence_id
-        JOIN employees e ON a.operator_name = e.employee_id
-          JOIN pumpsales_shift_data psd ON a.attendence_id=psd.attendence_id
-        WHERE ps.created_at BETWEEN $1 AND $2
-          AND e."employeeName" ILIKE $3
-        ORDER BY e.employee_id, ps.created_at DESC
-      `;
+        SELECT DISTINCT ON (a.attendence_id)
+          ps.pump_sale_id, 
+          ps.bay_side, 
+          ps.fuel_type, 
+          ps.created_at, 
+          a."pumpNumber", 
+          a.operatorshift, 
+          e."employeeName", 
+          a.attendence_id, 
+          e.employee_id,
+          psd.plus_amount,
+          psd.short_amount,
+          psd.shift_sales_amount,
+          psd.pump_sale_amount
+      FROM pump_sales ps
+      JOIN attendence a ON ps.attendence_id = a.attendence_id
+      JOIN employees e ON a.operator_name = e.employee_id
+      JOIN pumpsales_shift_data psd ON a.attendence_id = psd.attendence_id
+      WHERE ps.created_at BETWEEN $1 AND $2
+      AND e."employeeName" ILIKE $3
+      ORDER BY a.attendence_id, ps.created_at DESC;
+        `;
 
         const values = [fromDate, toDate, `%${employee_name}%`]; // Use ILIKE for case-insensitive search
 
